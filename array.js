3816ffaf036f383137193949eab04045ffd20b89
shivammaks <113494083+shivammaks@users.noreply.github.com>
2025-11-26 16:04:04 +0530

    Refactor sum_avg function variable naming

--- array.js
+++ array.js
@@ -1,12 +1,13 @@
 function sum_avg() {
     var reviews = [5, 5, 4.5, 1, 3];
-    var total = 0;
+    var totalsum = 0;
     for (var i = 0; i < reviews.length; i++) {
         console.log(reviews[i]);
-        total += reviews[i];
+        totalsum += reviews[i];
     }
-    var average = total / reviews.length;
+    var average = totalsum / reviews.length;
     console.log("review average=" + average);
     console.log(total);
 }
 sum_avg();
+
