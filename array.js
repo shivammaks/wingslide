389b2a28d23a0ceb3175e983609897238da734c9
shivammaks <113494083+shivammaks@users.noreply.github.com>
2025-11-26 16:02:19 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+function sum_avg() {
+    var reviews = [5, 5, 4.5, 1, 3];
+    var total = 0;
+    for (var i = 0; i < reviews.length; i++) {
+        console.log(reviews[i]);
+        total += reviews[i];
+    }
+    var average = total / reviews.length;
+    console.log("review average=" + average);
+    console.log(total);
+}
+sum_avg();
